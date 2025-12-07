8720059634932cae802f8fdd1785a16fe9b5a48a
akash <akashraval3709@gmail.com>
2025-12-07 09:14:08 +0530

    Add secure login system with brute force protection
    This Python script simulates a secure authentication mechanism.
    
    Key Features:
    1. Hardcoded Credential Check: Validates user input against a stored password.
    2. Attempt Limitation: Implements a loop to restrict the user to a maximum of 3 login attempts (Brute Force Protection).
    3. System Lockout: Logic to display a 'SYSTEM LOCKED' message if all attempts fail.
    4. User Feedback: Provides immediate feedback for incorrect attempts.
    
    This project demonstrates the use of 'loops', 'conditional statements', and 'break' keywords in Python.

---
+++ secure_login.py
@@ -0,0 +1,23 @@
+
+# Program: Secure Login System with 3 Attempts
+# Author: [Akash Raval]
+# Purpose: Basic password authentication logic for Cyber Security
+
+# Store the correct password
+password = "python123"
+
+# Loop to allow a maximum of 3 attempts
+for i in range(3):
+    # Prompt the user to enter the password
+    pin = input('\nenyter your password : ')
+# Validate the entered password
+    if pin == password:
+        print('Access Granted')
+        break  # Break the loop if authentication is successful
+    else:
+        # Check if it was the last attempt
+        if i != 2:
+            print('your password is wrong pleas try again........')
+        # Lock the system after 3 failed attempts
+        else:
+            print('SYSTEM LOCKED')
